219e2003d525b9fcf445643489da258310e3c606
Moinhausen <138520087+0xMoinhausen@users.noreply.github.com>
2024-07-01 08:28:16 +0200

    sfasdsad

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -51,10 +51,10 @@ float get_water_height();
 void playSound();
 
 // WLAN-Zugangsdaten
-const char *ssid = "FRITZ!Repeater 2400";
-const char *password = "75763178721406690002";
-// const char *ssid = "UBBST-WLAN";
-// const char *password = "Game2016";
+// const char *ssid = "FRITZ!Repeater 2400";
+// const char *password = "75763178721406690002";
+const char *ssid = "UBBST-WLAN";
+const char *password = "Game2016";
 
 // Melodie für den Buzzer
 const char *simpeons = "The Simpsons:d=4,o=5,b=160:c.6,e6,f#6,8a6,g.6,e6,c6,8a,8f#,8f#,8f#,2g,8p,8p,8f#,8f#,8f#,8g,a#.,8c6,8c6,8c6,c6";
